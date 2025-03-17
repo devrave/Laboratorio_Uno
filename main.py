8ff2d6ea9f54f9b8b5b13ee68e299f3ba4974ce4
Andres Cano Rave <116232866+devrave@users.noreply.github.com>
2025-03-17 10:47:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ Numeros divisibles 3 = Fizz
 Numeros divisibles 5 = Buzz
 Numeros divisibles 2 y 5 = Fizzbuzz"
 """
-""" Se utliza la función range que me permite introducir parametro de inicio y parametro de final,
-sin embargo nunca para en el parametro final, sino un numero antes, por ende se le debe poner un numero mayor"""
+""" Se utliza la función range que me permite introducir parametro de inicio y parametro final,
+sin embargo, siempre llega hasta un numero antes del parametro final, por ende se le debe poner un numero mas."""
 
 for num in range(1,1001):
     if num % 3 == 0 and num % 5 == 0:
